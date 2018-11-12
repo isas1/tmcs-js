4c3b93ed07047d3540ead6a018a2c874ff857ffa
isas1 <teachmecomputerscience@gmail.com>
2018-11-12 22:52:24 +0000

    Removed old code

--- js/main.js
+++ js/main.js
@@ -1,85 +1,10 @@
 /*eslint no-console: "off"*/
 /*eslint quote-props: "off"*/
-// ***** Img sliding ***** //
-
-// UX - Preventing too many request being made
-// function debounce(func, wait = 20, immediate = true) {
-//   var timeout;
-//   return function() {
-//     var context = this, args = arguments;
-//     var later = function() {
-//       timeout = null;
-//       if (!immediate) func.apply(context, args);
-//     };
-//     var callNow = immediate && !timeout;
-//     clearTimeout(timeout);
-//     timeout = setTimeout(later, wait);
-//     if (callNow) func.apply(context, args);
-//   };
-// }
-
-// const imgs = document.querySelectorAll('.pic');
-
-// window.addEventListener("scroll", debounce(checkSlide));
-
-// function checkSlide() {
-//   // forEach img in imgs
-//   imgs.forEach(function(img) {
-  
-//   // find the middle of img
-//   const slideIn = (window.scrollY + window.innerHeight) - img.height / 2 // start of transition
-//   // console.log(`Y: ${window.scrollY}`)
-//   // console.log(`InnerHeight: ${window.innerHeight}`)
-//   const imgBottom = img.offsetTop + img.height; // bottom of the image is the end of the transition
-//   // console.log(`imgBottom: ${imgBottom}`);
-//   // console.log(`img.offsetTop: ${img.offsetTop}`);
-//   const isHalfShown = slideIn > img.offsetTop; // between middle of image and bottom - to add "active" class
-//   // console.log(`isHalfShown: ${isHalfShown}`);
-//   const isNotPast = imgBottom > window.scrollY; // above bottom of image - to add "active" class
-//   // console.log(`isNotPast: ${isNotPast}`);
-
-//   if (isHalfShown && isNotPast) { // add 'active' class when slidIn
-//         img.classList.add("active");
-//       } else { // finish 'active' class when imgBottom
-//         img.classList.remove("active"); 
-//       }
-//   });
-// }
-
-
-// ***** Gallery ***** //
-
-const getPanels = document.querySelectorAll('.panel');
-    // link = document.querySelector('.link');
-    // str = '<a href="http://samisaacs.website">Portfolio</a>';
-    
-    // function to toggle .open class - this.addclass(.panels)
-    function toggleOpen() {
-      this.classList.toggle('open');
-      // console.log(link);
-      // link.outerHTML=str;
-    }
-    
-    // add onclick listener to run toggle function
-    getPanels.forEach(panel => {
-      panel.addEventListener("click", toggleOpen);
-    });
-    getPanels.forEach(panel => {
-      panel.addEventListener("transitionend", toggleActive);
-    });
-
-    // write a function to toggle an active class
-    function toggleActive(e) {
-      //if transistion includes 'flex'
-      if (e.propertyName.includes('flex')) {
-        this.classList.toggle('open-active');
-      }
-    }
-
 
 // ***** STICKY NAV ***** //
 const nav = document.querySelector('nav');
 const topOfNav = nav.offsetTop; // was let -eslint error
+console.log(window);
 
 function fixNav() {
   if (window.scrollY >= topOfNav) {
